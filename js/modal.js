2d9ffdd2ca9a9b5d683d9fb18b8245b37a3c8768
Maryna145 <marina.vosko@gmail.com>
2025-08-12 13:53:03 +0300

    position

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector('.backdrop')
-const modalBtnOpen = document.querySelector(".modal-Btn-Open");
-const modalBtnClose = document.querySelector(".modal-Btn-Close");
+const modalBtnOpen = document.querySelector(".modal-btn-open");
+const modalBtnClose = document.querySelector(".modal-btn-close");
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
